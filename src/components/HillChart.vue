dffae2a02bf001e49441b0f38b3acf788199f94f
Roman Alekseev <jesprider@gmail.com>
2026-06-07 16:12:32 +0200

    Keep ghost trails on project color while live dots redden.

--- src/components/HillChart.vue
+++ src/components/HillChart.vue
@@ -89,7 +89,7 @@ onBeforeUnmount(onUp)
         v-if="m.id === selectedId"
         :ghosts="m.ghosts"
         :radius="m.radius"
-        :color="m.color"
+        :color="m.baseColor"
       />
       <MarkerChart
         :cx="curveX(m.position)"
